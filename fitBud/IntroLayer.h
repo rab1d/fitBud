52d2a489db4e0e978d3558dfa23c063c28a44045
Mohammod Arafat <Arafat@Mohammod-Arafats-iMac.local>
2013-01-06 12:40:34 -0500

    Initial Commit

---
+++ fitBud/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  fitBud
+//
+//  Created by Mohammod Arafat on 1/6/13.
+//  Copyright CUNY City College 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ fitBud/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  fitBud
+//
+//  Created by Mohammod Arafat on 1/6/13.
+//  Copyright CUNY City College 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
