7e4cf9056476d61b553c87a183ab9c7f79c23fea
Mo Arafat <arafat59@gmail.com>
2013-02-25 20:33:50 -0500

    updated fitBud Class Diagram

---
+++ MainGameScene.h
@@ -0,0 +1,24 @@
+//
+//  MainGameScene.h
+//  fitBud
+//
+//  Created by Mohammod Arafat on 2/24/13.
+//  Copyright 2013 CUNY City College. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface MainGameScene : CCLayer {
+    
+}
+
+// @property double experiencePoints;
+@property (nonatomic) Slider *SliderLayer;
+@property 
+
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ MainGameScene.m
@@ -0,0 +1,16 @@
+//
+//  MainGameScene.m
+//  fitBud
+//
+//  Created by Mohammod Arafat on 2/24/13.
+//  Copyright 2013 CUNY City College. All rights reserved.
+//
+
+#import "MainGameScene.h"
+
+
+@implementation MainGameScene
+
+
+
+@end
